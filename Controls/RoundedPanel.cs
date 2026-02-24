086f5146953c983589b229fcba9675212edb70f5
csizmadiabence <csizmadiabencee@gmail.com>
2026-02-24 22:51:32 +0100

    2026.02.24 - WinForm Front End

---
+++ Controls/RoundedPanel.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Windows.Forms;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+
+namespace ticketmaster.Controls
+{
+public class RoundedPanel : Panel
+{
+    public int BorderRadius { get; set; } = 20;
+    public int BorderSize { get; set; } = 1;
+    public Color BorderColor { get; set; } = Color.Transparent;
+
+        private GraphicsPath GetFigurePath(RectangleF rect, int radius)
+        {
+            GraphicsPath path = new GraphicsPath();
+            float curveSize = radius * 2F;
+
+            path.StartFigure();
+            path.AddArc(rect.X, rect.Y, curveSize, curveSize, 180, 90);
+            path.AddArc(rect.Right - curveSize, rect.Y, curveSize, curveSize, 270, 90);
+            path.AddArc(rect.Right - curveSize, rect.Bottom - curveSize, curveSize, curveSize, 0, 90);
+            path.AddArc(rect.X, rect.Bottom - curveSize, curveSize, curveSize, 90, 90);
+            path.CloseFigure();
+
+            return path;
+        }
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            e.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+
+            // 1. A doboz vágása marad a ClientRectangle-ön
+            using (var pathRegion = GetFigurePath(this.ClientRectangle, BorderRadius))
+            {
+                this.Region = new Region(pathRegion);
+            }
+
+            // 2. A RAJZOLÁS ELTOLÁSA:
+            // A Y koordinátát 1.0f-re állítjuk (lejjebb toljuk), 
+            // és a magasságból (Height) is többet vonunk le, hogy alul se érjen ki.
+            RectangleF rect = new RectangleF(1.0f, 1.0f, this.Width - 2.1f, this.Height - 2.1f);
+
+            using (var path = GetFigurePath(rect, BorderRadius))
+            {
+                // Belső fehér rész
+                using (SolidBrush brush = new SolidBrush(Color.White))
+                {
+                    e.Graphics.FillPath(brush, path);
+                }
+
+                // Keret rajzolása
+                if (BorderSize > 0)
+                {
+                    using (Pen pen = new Pen(BorderColor, BorderSize))
+                    {
+                        pen.Alignment = PenAlignment.Inset;
+                        e.Graphics.DrawPath(pen, path);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ UC_Register.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace ticketmaster
+{
+    partial class UC_Register
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.EmailBoxReg = new System.Windows.Forms.TextBox();
+            this.useemail = new System.Windows.Forms.Label();
+            this.LoginText = new System.Windows.Forms.Label();
+            this.ProgramName = new System.Windows.Forms.Label();
+            this.CreateAccountButtonReg = new ticketmaster.Controls.RoundedButton();
+            this.termscumo = new System.Windows.Forms.RichTextBox();
+            this.PhoneNumber = new System.Windows.Forms.TextBox();
+            this.PasswordReg = new System.Windows.Forms.TextBox();
+            this.ConfirmPasswordReg = new System.Windows.Forms.TextBox();
+            this.Username = new System.Windows.Forms.TextBox();
+            this.RegisterPanel = new System.Windows.Forms.Panel();
+            this.picMin = new System.Windows.Forms.PictureBox();
+            this.picExit = new System.Windows.Forms.PictureBox();
+            this.BackButtonReg = new ticketmaster.Controls.RoundedButton();
+            this.RegisterPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picMin)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picExit)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // EmailBoxReg
+            // 
+            this.EmailBoxReg.Font = new System.Drawing.Font("Inter", 12F);
+            this.EmailBoxReg.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.EmailBoxReg.Location = new System.Drawing.Point(184, 188);
+            this.EmailBoxReg.Name = "EmailBoxReg";
+            this.EmailBoxReg.Size = new System.Drawing.Size(275, 27);
+            this.EmailBoxReg.TabIndex = 17;
+            // 
+            // useemail
+            // 
+            this.useemail.AutoSize = true;
+            this.useemail.Font = new System.Drawing.Font("Inter", 9.75F);
+            this.useemail.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.useemail.Location = new System.Drawing.Point(213, 108);
+            this.useemail.Name = "useemail";
+            this.useemail.Size = new System.Drawing.Size(217, 18);
+            this.useemail.TabIndex = 16;
+            this.useemail.Text = "Use your email or phone number.";
+            // 
+            // LoginText
+            // 
+            this.LoginText.AutoSize = true;
+            this.LoginText.Font = new System.Drawing.Font("Inter SemiBold", 14.25F, System.Drawing.FontStyle.Bold);
+            this.LoginText.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.LoginText.Location = new System.Drawing.Point(220, 82);
+            this.LoginText.Name = "LoginText";
+            this.LoginText.Size = new System.Drawing.Size(203, 27);
+            this.LoginText.TabIndex = 15;
+            this.LoginText.Text = "Create your account";
+            // 
+            // ProgramName
+            // 
+            this.ProgramName.AutoSize = true;
+            this.ProgramName.Font = new System.Drawing.Font("Inter", 18F, System.Drawing.FontStyle.Bold);
+            this.ProgramName.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.ProgramName.Location = new System.Drawing.Point(233, 11);
+            this.ProgramName.Name = "ProgramName";
+            this.ProgramName.Size = new System.Drawing.Size(176, 35);
+            this.ProgramName.TabIndex = 14;
+            this.ProgramName.Text = "Ticket Master";
+            // 
+            // CreateAccountButtonReg
+            // 
+            this.CreateAccountButtonReg.BackColor = System.Drawing.Color.Black;
+            this.CreateAccountButtonReg.FlatAppearance.BorderSize = 0;
+            this.CreateAccountButtonReg.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.CreateAccountButtonReg.Font = new System.Drawing.Font("Inter Medium", 8.25F, System.Drawing.FontStyle.Bold);
+            this.CreateAccountButtonReg.ForeColor = System.Drawing.Color.White;
+            this.CreateAccountButtonReg.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.CreateAccountButtonReg.Location = new System.Drawing.Point(184, 376);
+            this.CreateAccountButtonReg.Name = "CreateAccountButtonReg";
+            this.CreateAccountButtonReg.Size = new System.Drawing.Size(275, 30);
+            this.CreateAccountButtonReg.TabIndex = 18;
+            this.CreateAccountButtonReg.Text = "Create Account";
+            this.CreateAccountButtonReg.UseVisualStyleBackColor = false;
+            // 
+            // termscumo
+            // 
+            this.termscumo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(246)))), ((int)(((byte)(246)))), ((int)(((byte)(246)))));
+            this.termscumo.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.termscumo.Font = new System.Drawing.Font("Inter", 8.25F);
+            this.termscumo.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(130)))), ((int)(((byte)(130)))), ((int)(((byte)(130)))));
+            this.termscumo.Location = new System.Drawing.Point(195, 423);
+            this.termscumo.Name = "termscumo";
+            this.termscumo.ReadOnly = true;
+            this.termscumo.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None;
+            this.termscumo.Size = new System.Drawing.Size(253, 96);
+            this.termscumo.TabIndex = 19;
+            this.termscumo.Text = "By clicking continue, you agree to our Terms of Service and Privacy Policy";
+            // 
+            // PhoneNumber
+            // 
+            this.PhoneNumber.Font = new System.Drawing.Font("Inter", 12F);
+            this.PhoneNumber.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.PhoneNumber.Location = new System.Drawing.Point(184, 233);
+            this.PhoneNumber.Name = "PhoneNumber";
+            this.PhoneNumber.Size = new System.Drawing.Size(275, 27);
+            this.PhoneNumber.TabIndex = 20;
+            // 
+            // PasswordReg
+            // 
+            this.PasswordReg.Font = new System.Drawing.Font("Inter", 12F);
+            this.PasswordReg.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.PasswordReg.Location = new System.Drawing.Point(184, 278);
+            this.PasswordReg.Name = "PasswordReg";
+            this.PasswordReg.PasswordChar = '*';
+            this.PasswordReg.Size = new System.Drawing.Size(275, 27);
+            this.PasswordReg.TabIndex = 21;
+            // 
+            // ConfirmPasswordReg
+            // 
+            this.ConfirmPasswordReg.Font = new System.Drawing.Font("Inter", 12F);
+            this.ConfirmPasswordReg.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.ConfirmPasswordReg.Location = new System.Drawing.Point(184, 323);
+            this.ConfirmPasswordReg.Name = "ConfirmPasswordReg";
+            this.ConfirmPasswordReg.PasswordChar = '*';
+            this.ConfirmPasswordReg.Size = new System.Drawing.Size(275, 27);
+            this.ConfirmPasswordReg.TabIndex = 22;
+            // 
+            // Username
+            // 
+            this.Username.Font = new System.Drawing.Font("Inter", 12F);
+            this.Username.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.Username.Location = new System.Drawing.Point(184, 143);
+            this.Username.Name = "Username";
+            this.Username.Size = new System.Drawing.Size(275, 27);
+            this.Username.TabIndex = 23;
+            // 
+            // RegisterPanel
+            // 
+            this.RegisterPanel.Controls.Add(this.BackButtonReg);
+            this.RegisterPanel.Controls.Add(this.Username);
+            this.RegisterPanel.Controls.Add(this.ProgramName);
+            this.RegisterPanel.Controls.Add(this.ConfirmPasswordReg);
+            this.RegisterPanel.Controls.Add(this.termscumo);
+            this.RegisterPanel.Controls.Add(this.PasswordReg);
+            this.RegisterPanel.Controls.Add(this.CreateAccountButtonReg);
+            this.RegisterPanel.Controls.Add(this.PhoneNumber);
+            this.RegisterPanel.Controls.Add(this.LoginText);
+            this.RegisterPanel.Controls.Add(this.EmailBoxReg);
+            this.RegisterPanel.Controls.Add(this.useemail);
+            this.RegisterPanel.Location = new System.Drawing.Point(196, 71);
+            this.RegisterPanel.Name = "RegisterPanel";
+            this.RegisterPanel.Size = new System.Drawing.Size(822, 570);
+            this.RegisterPanel.TabIndex = 24;
+            // 
+            // picMin
+            // 
+            this.picMin.BackColor = System.Drawing.Color.Transparent;
+            this.picMin.Image = global::ticketmaster.Properties.Resources.minimizeicon;
+            this.picMin.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.picMin.Location = new System.Drawing.Point(982, 12);
+            this.picMin.Name = "picMin";
+            this.picMin.Size = new System.Drawing.Size(15, 15);
+            this.picMin.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picMin.TabIndex = 25;
+            this.picMin.TabStop = false;
+            this.picMin.Click += new System.EventHandler(this.picMin_Click);
+            this.picMin.Paint += new System.Windows.Forms.PaintEventHandler(this.picMin_Paint);
+            this.picMin.MouseEnter += new System.EventHandler(this.picMin_MouseEnter);
+            this.picMin.MouseLeave += new System.EventHandler(this.picMin_MouseLeave);
+            // 
+            // picExit
+            // 
+            this.picExit.Image = global::ticketmaster.Properties.Resources.exiticon;
+            this.picExit.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.picExit.Location = new System.Drawing.Point(1003, 12);
+            this.picExit.Name = "picExit";
+            this.picExit.Size = new System.Drawing.Size(15, 15);
+            this.picExit.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picExit.TabIndex = 24;
+            this.picExit.TabStop = false;
+            this.picExit.Click += new System.EventHandler(this.picExit_Click);
+            this.picExit.Paint += new System.Windows.Forms.PaintEventHandler(this.picExit_Paint);
+            this.picExit.MouseEnter += new System.EventHandler(this.picExit_MouseEnter);
+            this.picExit.MouseLeave += new System.EventHandler(this.picExit_MouseLeave);
+            // 
+            // BackButtonReg
+            // 
+            this.BackButtonReg.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.BackButtonReg.FlatAppearance.BorderSize = 0;
+            this.BackButtonReg.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BackButtonReg.Font = new System.Drawing.Font("Inter SemiBold", 8.25F, System.Drawing.FontStyle.Bold);
+            this.BackButtonReg.ForeColor = System.Drawing.Color.White;
+            this.BackButtonReg.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.BackButtonReg.Location = new System.Drawing.Point(715, 518);
+            this.BackButtonReg.Name = "BackButtonReg";
+            this.BackButtonReg.Size = new System.Drawing.Size(72, 32);
+            this.BackButtonReg.TabIndex = 24;
+            this.BackButtonReg.Text = "Go back →";
+            this.BackButtonReg.UseVisualStyleBackColor = false;
+            this.BackButtonReg.Click += new System.EventHandler(this.BackButtonReg_Click);
+            // 
+            // UC_Register
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(246)))), ((int)(((byte)(246)))), ((int)(((byte)(246)))));
+            this.Controls.Add(this.picMin);
+            this.Controls.Add(this.RegisterPanel);
+            this.Controls.Add(this.picExit);
+            this.Name = "UC_Register";
+            this.Size = new System.Drawing.Size(1035, 655);
+            this.RegisterPanel.ResumeLayout(false);
+            this.RegisterPanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picMin)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picExit)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox EmailBoxReg;
+        private System.Windows.Forms.Label useemail;
+        private System.Windows.Forms.Label LoginText;
+        private System.Windows.Forms.Label ProgramName;
+        private Controls.RoundedButton CreateAccountButtonReg;
+        private System.Windows.Forms.RichTextBox termscumo;
+        private System.Windows.Forms.TextBox PhoneNumber;
+        private System.Windows.Forms.TextBox PasswordReg;
+        private System.Windows.Forms.TextBox ConfirmPasswordReg;
+        private System.Windows.Forms.TextBox Username;
+        private System.Windows.Forms.Panel RegisterPanel;
+        private System.Windows.Forms.PictureBox picMin;
+        private System.Windows.Forms.PictureBox picExit;
+        private Controls.RoundedButton BackButtonReg;
+    }
+}
