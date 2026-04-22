487f58001f873c3feae6baefd5fb82ef092bac04
maaszd <maaszdavid2000@gmail.com>
2026-04-22 19:13:46 +0200

    Theaterrow külön osztályt kapott, +Cinemahall osztály

---
+++ backend/backend/Models/Theaterrow.cs
@@ -0,0 +1,10 @@
+﻿namespace backend.Models;
+
+using System.Collections.Generic;
+
+public class TheaterRow
+{
+    public int Id { get; set; } 
+    public int RowNumber { get; set; }
+    public List<Seat> Seats { get; set; } = new();
+}
--- backend/backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53f60aec8cdf73355453efed37eac8e38102b915")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+468a58ae4991b3ab4176f9db5359f012130f24e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
