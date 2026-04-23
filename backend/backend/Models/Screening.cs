39a83ec024b87b4644fff908499c26b794ce86a3
morci <csizmadiabencee@gmail.com>
2026-04-23 21:56:42 +0200

    Végleges leszámolás a bin és obj mappákkal

--- backend/backend/Models/Screening.cs
+++ backend/backend/Models/Screening.cs
@@ -11,14 +11,9 @@ public class Screening
     public int CinemaHallId { get; set; }
     public DateTime StartTime { get; set; }
     public decimal Price { get; set; }
+
     public Movie? Movie { get; set; }
 
     [JsonIgnore]
-<<<<<<< HEAD
-=======
-    public Movie? Movie { get; set; }
-
-    [JsonIgnore]
->>>>>>> 4461f0d188361205378678edd9851f3ebd202252
     public CinemaHall? CinemaHall { get; set; }
 }
